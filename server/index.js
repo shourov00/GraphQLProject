9f1b506413cfa8133075ec78f7e67bf208102a43
Shourov00 <sfazleyrabbi@gmail.com>
2023-10-26 12:37:59 +0600

    Graphql & DB setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+require('colors')
 require('dotenv').config();
-const { graphqlHTTP } = require('express-graphql')
-const port = process.env.PORT || 5000;
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema')
+const connectDB = require('./config/db')
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+// Connect to Database
+connectDB()
 
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: process.env.NODE_ENV === 'development'
 }))
 
-app.listen(port, console.log(`Server running on port ${port}`))
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port: ${port}`)
+})
 
